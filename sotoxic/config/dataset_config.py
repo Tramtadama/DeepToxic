3c1c345db39d312a42c0d5d02043237f93585121
ÂTramtadama <kakadustew@seznam.cz>
2019-04-04 20:35:36 +0200

    a

--- sotoxic/config/dataset_config.py
+++ sotoxic/config/dataset_config.py
@@ -5,6 +5,6 @@ SAMPLE_SUBMISSION_PATH = '../input/jigsaw-unintended-bias-in-toxicity-classifica
 
 # for feature's path
 CLEAN_WORDS_PATH = 'features/cleanwords.txt'
-FASTTEXT_PATH = '../input/FastText-crawl-300d-2M/crawl-300d-2M.vec'
+FASTTEXT_PATH = '../input/fasttext-crawl-300d-2m/crawl-300d-2M.vec'
 GLOVE_PATH = '../input/glove.840B.300d.txt/glove.840B.300d.txt'
 
