0c694e565d21bd4df76c7f44418f7c9542851df8
ÂTramtadama <kakadustew@seznam.cz>
2019-04-04 20:15:58 +0200

    paths fixd

--- sotoxic/config/dataset_config.py
+++ sotoxic/config/dataset_config.py
@@ -1,10 +1,10 @@
 # for dataset's path
-TRAIN_PATH = '../Data/train_abl.csv'
-TEST_PATH = '../Data/test.csv'
-SAMPLE_SUBMISSION_PATH = '../Data/sample_submission.csv'
+TRAIN_PATH = '../input/trainabl/train_abl.csv'
+TEST_PATH = '../input/jigsaw-unintended-bias-in-toxicity-classification/test.csv'
+SAMPLE_SUBMISSION_PATH = '../input/jigsaw-unintended-bias-in-toxicity-classification/sample_submission.csv'
 
 # for feature's path
 CLEAN_WORDS_PATH = 'features/cleanwords.txt'
-FASTTEXT_PATH = 'features/crawl-300d-2M.vec'
-GLOVE_PATH = 'features/glove.840B.300d.txt'
+FASTTEXT_PATH = '../input/crawl-300d-2M.vec/crawl-300d-2M.vec'
+GLOVE_PATH = '../input/glove.840B.300d/glove.840B.300d.txt'
 
